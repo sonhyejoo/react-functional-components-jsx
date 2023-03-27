a684ca467757eefb659fc3f400b41ffc671081a8
Son Hyejoo <107318155+sonhyejoo@users.noreply.github.com>
2023-03-27 15:29:21 -0400

    done

--- src/Showcase.js
+++ src/Showcase.js
@@ -1,16 +1,20 @@
-import BulbJpg from './images/bulbasaur.jpg'
+import BulbJpg from "./images/bulbasaur.jpg";
 
 function Showcase() {
-    const favPokemon = 'Bulbasaur'
-    const pokeChars = {
-        type: 'Grass',
-        move: 'Vine Whip'
-    }
+  const favPokemon = "Bulbasaur";
+  const pokeChars = {
+    type: "Grass",
+    move: "Vine Whip"
+  };
   return (
     <div>
       <h1>{favPokemon}'s Showcase Component</h1>
       <img src={BulbJpg} alt={favPokemon}></img>
-      <h2>{favPokemon}'s Type is <span style={{backgroundColor: 'green'}}>{pokeChars.type}</span> and Move is <span>{pokeChars.move}</span></h2>
+      <h2>
+        {favPokemon}'s Type is{" "}
+        <span style={{ backgroundColor: "green" }}>{pokeChars.type}</span> and
+        Move is <span>{pokeChars.move}</span>
+      </h2>
     </div>
   );
 }
